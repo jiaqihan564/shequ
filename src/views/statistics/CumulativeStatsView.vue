9ff9cf109f4718961873ac1b14adda8134b0b4da
JiaQiHan <qihan.jia@icesimba.cn>
2025-11-15 01:16:09 +0800

    fix(statistics): 修复安全统计数据过滤逻辑

--- src/views/statistics/CumulativeStatsView.vue
+++ src/views/statistics/CumulativeStatsView.vue
@@ -80,7 +80,7 @@
         </template>
         <el-row :gutter="20">
           <el-col
-            v-for="item in data.security.filter((i: any) => i.stat_key !== 'blocked_ips' && i.stat_key !== 'security_alerts')"
+            v-for="item in data.security"
             :key="item.stat_key"
             :xs="24"
             :sm="12"
